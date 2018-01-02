56d63d353c96c7ffd1010c0a71383d888feee059
Gregor Richardson <gregor.richardson@gmail.com>
2018-01-01 20:11:05 -0800

    basic house API example

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,92 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions'); //for firebase storage
+const firebase = require('firebase-admin'); //for firebase database
 const express = require('express'); // load express from node_modules
+const engines = require('consolidate'); // allows popular templating libaries to work with Express
+
 const app = express(); // instantiate express to use its functionality
+
+app.engine('hbs', engines.handlebars); // use consolidate to attach handlebars templating
+app.set('views', './views'); // set views folder to our directory
+app.set('view engine', 'hbs'); // use our engine
 
 const bodyParser = require('body-parser'); // go inside message body
 
 const PORT = process.env.PORT || 3000; // set a port. Look to environment variable if avaialble
+
+const firebaseApp = firebase.initializeApp(
+    functions.config().firebase // use credentials from configured project
+);
+
+function getHouses() {
+    const ref = firebaseApp.database().ref('houses'); //firebase database
+    console.log('inside getHouses');
+    return ref.once('value').then(snap => snap.val());
+}
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
 // ROUTES
+
+// all via JSON
+// app.get('/all', (request, response) => {
+//     console.log("showing all houses")
+//     getHouses().then(house=> {
+//         response.json({houses :house})
+//     });
+// });
+
+// all via HTML
+app.get('/all', (request, response) => {
+    console.log("showing all houses")
+    getHouses().then(house => {
+        console.log('returning a house');
+        response.render('index', { house }); // render index page and send back data in house var
+    });
+});
+
+// caching example. Useful for portfolio app, but House app
+// app.get('/all', (request, response) => {
+//     console.log("showing all houses")
+//     response.set('Cache-Control', 'public, max-age=300, s-maxage=600'); //enable firebase caching. Max-age in seconds
+//     getHouses().then(house => {
+//         response.json({houses : house})
+//     });
+// });
+
+app.post('/new', (request, response) => {
+    console.log("Making a new house")
+    const db = firebaseApp.database().ref('houses'); //firebase database
+    console.log(request.body);
+    let {name, address, color, rooms, garage} = request.body;
+    let item = {
+        "name": name,
+        "address": address,
+        "color": color,
+        "rooms": rooms,
+        "garage": garage
+    }
+    db.push(item); // submit items
+    response.send(`${name} house created`)
+});
+
+app.get('/new', (request, response) => {
+    console.log("serving new house HTML page")
+    response.sendFile('add.html', {root: '../public'});
+});
+
+app.get('/index', (request, response) => {
+    resp.sendFile('index.html', {root: '../public'});
+});
+
 app.get('/hello', (request, response) => {
-    console.log("a request came in")
-    response.json({message: "Welcome to my API"})
+    console.log("a hello request came in")
+    response.json({message: "Welcome to my API!"})
     // response.send(`welcome to my API`)
 });
+
 
 app.get('/timestamp', (request, response) => {
     response.set('Cache-Control', 'public, max-age=300, s-maxage=600'); //enable firebase caching. Max-age in seconds
@@ -24,7 +95,16 @@ app.get('/timestamp', (request, response) => {
     response.send(`${Date.now()}`)
 });
 
+app.get('*', (request, response) => {
+    console.log('unknown route request');
+    response.send('Sorry, unknown route.');
+});
 
+app.post('*', (request, response) => {
+    console.log('unknown post route request');
+    response.send('Sorry, unknown post route.');
+});
+//app.get('*', (req, res) => res.redirect(CLIENT_URL)); // listen for all routes, send to homepage
 
 // Start the app so it listens for changes via Express
 // app.listen(PORT, () => console.log(`Listening on ${PORT}`));
---
+++ public/js/app.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var app = {};
+// var __API_URL__ = 'https://houseapidemo.firebaseapp.com/'; // deployed URL
+var __API_URL__ = 'http://localhost:5000'; // local URL
+
+(function(module) {
+    let newHouse = document.querySelector('#newForm');
+    if (newHouse) {
+        newHouse.addEventListener('submit',function(event){
+            event.preventDefault();
+            console.log('submit button was clicked');
+            let house = {
+                name: $('#name').val(),
+                address: $('#address').val(),
+                color: $('#color').val(),
+                rooms: $('#rooms').val(),
+                garage: $('#garage').val()
+            };
+            console.log(house);
+            insertRecord(house);
+            clearForm();
+        });
+    }
+    function insertRecord(house) {
+        $.post(`${__API_URL__}/new`, house)
+            .then(console.log('inserting new house'))
+            .then(() => console.log('House created!'))
+    }
+    function clearForm(){
+        feedbackMessage('Your house has been submitted!');
+        $('#name').val('');
+        $('#address').val('');
+        $('#color').val('');
+        $('#rooms').val('');
+        $('#garage').val('');
+    }
+    function errorCallback(err) {
+        console.error(err);
+        module.errorView.initErrorPage(err);
+    }
+
+    function feedbackMessage(message) {
+        document.querySelector('#feedback').innerHTML = message;
+        setTimeout(function() {
+        //console.log('delay before hiding');
+          document.querySelector('#feedback').innerHTML = '';
+        }, 3000);
+      }
+})(app);
+
+
+// $('#new-book-form').on('submit', bookView.addBook);
