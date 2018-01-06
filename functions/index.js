1c3f528a844f6435d5efcb0d1c6c01046a1ab95a
Gregor Richardson <gregor.richardson@gmail.com>
2018-01-05 21:45:01 -0800

    reinstates nasdaq-finance

--- functions/index.js
+++ functions/index.js
@@ -11,10 +11,10 @@ const express = require('express'); // load express from node_modules
 const engines = require('consolidate'); // allows popular templating libaries to work with Express
 const cors = require('cors'); // avoid cross browser scripting errors
 const bodyParser = require('body-parser'); // go inside message body
-const traderjs = require('traderjs');
-// const nf = require('nasdaq-finance'); // stock API
+// const traderjs = require('traderjs'); // dont need. dont want.
+const nf = require('nasdaq-finance'); // stock API
 // instructions: import nf from 'nasdaq-finance'; const nf = new NasdaqFinance()
-// const stock = new NasdaqFinance();
+const stock = new nf.default();
 // const stock = nf.NasdaqFinance();
 // const stock = nf.default; // on the right track?
 
@@ -48,24 +48,34 @@ function findStockData(asset) {
     console.log('inside stock asset', asset);
     console.log('this is asset symbol', asset.symbol);
     stock.getInfo(asset.symbol)
-    .then((response) => {    
-        console.log('inside asset response build...')
-        // capture 24h change, 24h gain, current price
-        // append data to existing object & return 
+    .then((response) => {
+        console.log('asset name: ',response.name)
+        asset.exchange = response.exchange;
+        asset.price = response.price;
+        asset.priceChange = response.priceChange;
+        asset.priceChangePercent = response.priceChangePercent;
+        console.log('new stock asset', asset);
+        console.log('returning new stock');
+        return asset;
     })
     .catch(console.error)
-    // return asset;
 }
 
 function findCryptoData(asset) {
     console.log('inside crypto function', asset);
-    stock.getInfo(asset.symbol)
-    .then((response) => {    
+    //stock.getInfo(asset.symbol)
+    //.then((response) => {    
         console.log('inside asset response build...')
         // capture 24h change, 24h gain, current price
         // append data to existing object & return 
-    })
-    .catch(console.error)
+        asset.price = 333.33,
+        asset.priceChange = 42.42,
+        asset.priceChangePercent = 42.50
+        console.log('new crypto asset', asset)
+        console.log('returning new crypto')
+        return asset;
+    //})
+    // .catch(console.error)
     // return asset;
 }
 
@@ -120,30 +130,37 @@ function findCryptoData(asset) {
 app.get('/all', (request, response) => {
     console.log("showing all assets route")
     getAssets().then(asset => {
+        let newStats;
         for (let i = 0;i<asset.length;i++) {
+            console.log('starting engines')
             console.log(asset[i].type);
-            if (asset[i].type=='stock') findStockData(asset[i]); // console.log(asset[i].name +' is a stock');//
-            else if (asset[i].type=='crypto') findCryptoData(asset[i]); //console.log(asset[i].name + ' is crypto');
-            response.json({assets : asset}) // send basic json response
+            // if (asset[i].type=='stock') {newStats = findStockData(asset[i]);console.log('newStats value ',newStats)} // console.log(asset[i].name +' is a stock');
+            if (asset[i].type=='stock') {sendReply(()=> response.send(findStockData(asset[i])))} //findStockData(asset[i])
+            //if (asset[i].type=='stock') findStockData(asset[i]); // console.log(asset[i].name +' is a stock');//
+            // else if (asset[i].type=='crypto') {newStats = findCryptoData(asset[i]);console.log('newStats value ',newStats)} 
+            else if (asset[i].type=='crypto') {response.send('something crypto new')} //findCryptoData(asset[i])
+            //console.log(asset[i].name + ' is crypto');
+            //response.json({assets : asset}) // send basic json response
+            console.log('almost ready for a response...')
+            // response.send('hello':'random','assetInfo': newStats.price)
+            // response.send(newStats.price)
        }
         // response.render('index', { asset }); // render index page and send back data in asset var
     });
 });
 // test bench for route 1...
 app.get('/test', (request, response) => {
-    var configuration = {
-        symbol: 'NASD:GOOG',
-        interval: 86400,
-        period: '2d',
-        fields: ['d','o','c','l','h','v']
-    };
-    traderjs
-        .config(configuration)
-        .transformer('json')
-        .temporal(function(data) {
-            console.log(data)
-            response.send(data)
+    stock.getInfo('AAPL')
+    .then((res) => {
+        console.log('apple price: ',res)
+        response.json({
+            'exchange': res.exchange,
+            'price': res.price,
+            'priceChange' : res.priceChange,
+            'priceChangePercent' : res.priceChangePercent
         })
+    })
+    .catch(console.error)
     // response.send(stock.getInfo('TSLA'));
     
 });
